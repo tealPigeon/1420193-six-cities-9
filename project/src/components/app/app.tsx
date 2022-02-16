009b84d2399215141e65aa52d7b5d85c1d2b09a8
Motoshkova Daria <motoshkovad@yandex.ru>
2022-02-16 05:13:12 +0500

    2.11. Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,23 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import React from 'react';
+import Main from '../main/main';
+// import PropertyNotLogged from '../property-not-logged/property-not-logged';
+// import Property from '../property/property';
+// import Favorites from '../favorites/favorites';
+// import Login from '../login/login';
+// import MainEmpty from '../main-empty/main-empty';
+// import FavoritesEmpty from '../favorites-empty/favorites-empty';
+type AppProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppProps): JSX.Element {
+  return <Main offersCount={offersCount}/>;
+  // return <PropertyNotLogged/>;
+  // return <Property/>;
+  // return <Favorites/>;
+  // return <Login/>;
+  // return <MainEmpty/>;
+  // return <FavoritesEmpty/>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const props = {
+  offersCount: 312,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App offersCount = {props.offersCount} />
   </React.StrictMode>,
   document.getElementById('root'));
